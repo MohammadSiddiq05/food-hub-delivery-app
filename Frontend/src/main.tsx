52989475c3957100a47e73d3a405599bca69b931
Muhammad Siddiq Shah <siddiqshah478@gmail.com>
2026-09-08 01:16:18 +0500

    Create app context for golbaly sharing user data

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AppContextProvider } from './context/AppContext.tsx';
 
 export const authService = "http://localhost:5000"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="66028336425-nds509qa8a972t8p7isprtsodtcfhkv4.apps.googleusercontent.com">
-      <App />
+     <AppContextProvider>
+       <App />
+     </AppContextProvider>
     </GoogleOAuthProvider>;
 
   </StrictMode>,
