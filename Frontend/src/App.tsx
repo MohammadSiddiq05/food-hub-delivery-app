d3ad95ba5ed70788c5eb0e13a7562d4567a15081
Muhammad Siddiq Shah <siddiqshah478@gmail.com>
2026-09-09 02:21:32 +0500

    add logout functionality

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,6 +6,7 @@ import SelectRole from "./pages/SelectRole";
 import PublicRoute from "./components/PublicRoute";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
+import Account from "./pages/Account";
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Home />} />
             <Route path="/select-role" element={<SelectRole />} />
+            <Route path="/account" element={<Account/>}/>
           </Route>
         </Routes>
         <Toaster />
